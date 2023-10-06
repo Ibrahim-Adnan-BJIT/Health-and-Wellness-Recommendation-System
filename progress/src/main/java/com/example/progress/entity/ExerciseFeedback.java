bc013993fdc490b58db70111974d3edeb8ae00f7
MallikaBJIT <mallika.dey@bjitacademy.com>
2023-10-06 12:14:53 +0600

    added service for dression analysis
    Task #481469

--- progress/src/main/java/com/example/progress/entity/ExerciseFeedback.java
+++ progress/src/main/java/com/example/progress/entity/ExerciseFeedback.java
@@ -18,7 +18,4 @@ public class ExerciseFeedback extends FeedbackParent{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-//    private long userId;
-//    private double rating;
-//    private String review;
 }
