a1938d14d1576bfd912d7e6eea08577abf0e2ec9
MallikaBJIT <mallika.dey@bjitacademy.com>
2023-10-06 08:53:30 +0600

    fix nutrition
    Task #481384

--- nutritionservice/src/main/java/com/example/nutritionservice/repository/FoodNutritionRepository.java
+++ nutritionservice/src/main/java/com/example/nutritionservice/repository/FoodNutritionRepository.java
@@ -4,4 +4,5 @@ import com.example.nutritionservice.entity.FoodNutrition;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FoodNutritionRepository extends JpaRepository<FoodNutrition, Integer> {
+    boolean existsByFood_IdAndNutrition_Id(Long foodId, Long nutritionId);
 }
